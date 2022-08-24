b8801a163caafc28e4876654c6cfc99d2c1f3d51
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-24 15:40:41 +0300

    add task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Напишите трёх значное число: ");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = number1 / 10;
+int number3 = number2 % 10;
+Console.WriteLine($"{number3}");
